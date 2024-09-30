248582a1621d9bcdacc19ad6a461f7e788a7a27d
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-30 17:36:24 -0600

    17. Object Types

--- setup/app.ts
+++ setup/app.ts
@@ -1,27 +1,12 @@
 (() => {
-  function add(
-    n1: number,
-    n2: number,
-    showResult: boolean,
-    phrase: string
-  ): void | number {
-    // if (typeof n1 !== "number" || typeof n2 !== "number") {
-    //   throw new Error("Incorrect input!");
-    // }
-    const result = n1 + n2;
+  // const oerson = {
+  //   name: String,
+  //   age: Number,
+  // };
+  const person = {
+    name: "John",
+    age: 30,
+  };
 
-    if (showResult) {
-      console.log(phrase + result);
-    } else {
-      return result;
-    }
-  }
-
-  let number1: number;
-  number1 = 5;
-  const number2 = 2.8;
-  const printResult = true;
-  const resultPhrase = "Result is: ";
-
-  add(number1, number2, printResult, resultPhrase);
+  console.log(person.name);
 })();
