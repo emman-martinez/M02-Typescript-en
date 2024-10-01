d2d10be61a7fd0e52a0c806815dfb302523ede44
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-30 18:05:02 -0600

    19. Arrays Types

--- setup/app.ts
+++ setup/app.ts
@@ -6,7 +6,15 @@
   const person = {
     name: "John",
     age: 30,
+    hobbies: ["Sports", "Cooking"],
   };
 
+  let favoriteActivities: string[];
+  favoriteActivities = ["Sports"];
+
   console.log(person.name);
+
+  for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+  }
 })();
