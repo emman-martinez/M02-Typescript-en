ac8b5365ddbba668203c7b3077c0ef942cb5647b
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-30 19:16:50 -0600

    21. Working witn Enums

--- setup/app.ts
+++ setup/app.ts
@@ -3,16 +3,34 @@
   //   name: String,
   //   age: Number,
   // };
-  const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-  } = {
+
+  // const person: {
+  //   name: string;
+  //   age: number;
+  //   hobbies: string[];
+  //   role: [number, string];
+  // } = {
+  //   name: "John",
+  //   age: 30,
+  //   hobbies: ["Sports", "Cooking"],
+  //   role: [2, "author"],
+  // };
+
+  // const ADMIN = 0;
+  // const READ_ONLY = 1;
+  // const AUTHOR = 2;
+
+  enum Role {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR,
+  }
+
+  const person = {
     name: "John",
     age: 30,
     hobbies: ["Sports", "Cooking"],
-    role: [2, "author"],
+    role: Role.AUTHOR,
   };
 
   // person.role.push("admin");
@@ -27,5 +45,9 @@
 
   for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
+  }
+
+  if (person.role === Role.AUTHOR) {
+    console.log("is author");
   }
 })();
