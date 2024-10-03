30847335bb41e7a58c4915202ed6ca99b66ecf1e
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-10-03 15:25:43 -0600

    31. The never Type

--- setup/app.ts
+++ setup/app.ts
@@ -7,4 +7,11 @@
   if (typeof userInput === "string") {
     userName = userInput;
   }
+
+  function generateError(message: string, code: number): never {
+    throw { message: message, errorCode: code };
+    // while (true) {}
+  }
+
+  generateError("An error occurred!", 500);
 })();
