5f20b8e3feac1045b1e512e8d4fade5fc96962bc
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-10-03 20:49:17 -0600

    63. ''private' and 'public' Access Modifiers

--- 04-cl-interfaces/src/app.ts
+++ 04-cl-interfaces/src/app.ts
@@ -1,6 +1,7 @@
 (() => {
   class Departament {
-    name: string;
+    public name: string;
+    private employees: string[] = [];
 
     constructor(n: string) {
       this.name = n;
@@ -9,12 +10,26 @@
     describe(this: Departament) {
       console.log("Departament: " + this.name);
     }
+
+    addEmployee(employee: string) {
+      this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+      console.log(this.employees.length);
+      console.log(this.employees);
+    }
   }
 
   const accounting = new Departament("Accounting");
   // console.log(accounting);
+  accounting.addEmployee("Emmanuel");
+  accounting.addEmployee("Jesús");
+  // accounting.employees[2] = "Anna";
   accounting.describe();
+  accounting.name = "NEW NAME";
+  accounting.printEmployeeInformation();
 
-  const accountingCopy = { name: "DUMMY", describe: accounting.describe };
-  accountingCopy.describe();
+  //   const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+  //   accountingCopy.describe();
 })();
