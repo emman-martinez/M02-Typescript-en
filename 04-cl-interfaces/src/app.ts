56d5e6a0aa3b7d8884c3a3170f3e64c644642d57
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-10-03 20:23:20 -0600

    60. Creating a First Class

---
+++ 04-cl-interfaces/src/app.ts
@@ -0,0 +1,12 @@
+(() => {
+  class Departament {
+    name: string;
+
+    constructor(n: string) {
+      this.name = n;
+    }
+  }
+
+  const accounting = new Departament("Accounting");
+  console.log(accounting);
+})();
