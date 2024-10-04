6c6ec9bd03ac02f17acaf23bed8a76a0f8309ead
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-10-03 20:36:03 -0600

    62. Constructor Functions & The 'this' Keyword

--- 04-cl-interfaces/src/app.ts
+++ 04-cl-interfaces/src/app.ts
@@ -5,8 +5,16 @@
     constructor(n: string) {
       this.name = n;
     }
+
+    describe(this: Departament) {
+      console.log("Departament: " + this.name);
+    }
   }
 
   const accounting = new Departament("Accounting");
-  console.log(accounting);
+  // console.log(accounting);
+  accounting.describe();
+
+  const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+  accountingCopy.describe();
 })();
