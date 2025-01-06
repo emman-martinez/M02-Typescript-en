30235ebcb91693f3053abbc536ebd7579f0f7a37
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2025-01-06 17:59:31 -0600

    79. Optional Parameters & Properties

--- 04-cl-interfaces/src/app.ts
+++ 04-cl-interfaces/src/app.ts
@@ -1,17 +1,18 @@
 (() => {
   // type AddFn = (a: number, b: number) => number;
-  interface AddFn {
-    (a: number, b: number): number;
-  }
+  // interface AddFn {
+  //   (a: number, b: number): number;
+  // }
 
-  let add: AddFn;
+  // let add: AddFn;
 
-  add = (n1: number, n2: number) => {
-    return n1 + n2;
-  };
+  // add = (n1: number, n2: number) => {
+  //   return n1 + n2;
+  // };
 
   interface Named {
-    readonly name: string;
+    readonly name?: string;
+    outputName?: string;
   }
 
   interface Greatable extends Named {
@@ -19,16 +20,24 @@
   }
 
   class Person implements Greatable {
-    constructor(public name: string, public age: number) {}
+    constructor(public name?: string, public age: number = 30) {
+      if (name) {
+        this.name = name;
+      }
+    }
 
     greet(phrase: string) {
-      console.log(phrase + " " + this.name);
+      if (this.name) {
+        console.log(`${ phrase } ${ this.name }`);
+      } else {
+        console.log("Hi!");
+      }
     }
   }
 
   let user1: Greatable;
 
-  user1 = new Person("Emmanuel", 30);
+  user1 = new Person();
 
   user1.greet("Hi there - I am");
   console.log(user1);
